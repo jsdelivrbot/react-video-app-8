64833117730dbc268b0c15f4a0f98545f0adb6fc
timogoudzwaard <timo.goudzwaard@gmail.com>
2018-05-12 15:36:35 +0200

    Add API keys to separate file

---
+++ API_keys.js
@@ -0,0 +1,3 @@
+// In normal projects you don't want to include this in your repo for security reasons.
+export const youtube = 'AIzaSyDJ9IGc9ykT78q5R1U9892buGdK8QMkCSc';
+export const maps = 'testkey';
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Keys from '../API_keys';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyDJ9IGc9ykT78q5R1U9892buGdK8QMkCSc';
+console.log(Keys);
 
 const App = () => {
     return (
